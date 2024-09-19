f9fe681a8ff820fdeed1d7b1acda640163b402d8
i23332 <i23332@verisk.com>
2024-09-19 08:25:03 -0600

    company list

---
+++ src/app/company/company-list/company-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Company } from '../../models/company';
+import { CompanyService } from '../company.service';
+
+@Component({
+  selector: 'app-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrl: './company-list.component.css'
+})
+export class CompanyListComponent implements OnInit {
+
+  public companies$: Observable<Company[]> | undefined;
+
+  constructor(private companyService: CompanyService) {
+  }
+
+  ngOnInit() {
+    this.getCompanies();
+  }
+
+  getCompanies() {
+    this.companies$ = this.companyService.getCompaniesObservable();
+  }
+
+}
